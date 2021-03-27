79fe917bedd6ba1eb1c34ad1e7fa39a07e511726
matteo-d <matteodrouin33@gmail.com>
2021-03-27 17:18:57 +0100

    lrllr

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,7 +1,25 @@
 const cacheName = "cache-v1";
 const assets = [
   "/",
-  "/index.html",
+  "fonts/fontawesome-webfont.woff2?v=4.7.0",
+  "fonts/et-line.woff",
+  "img/la-chouette-agence-banniere.webp",
+  "img/image-de-presentation.jpg",
+  "js/jquery-2.1.0.js",
+  "css/bootstrap.css",
+  "img/magazine.jpg",
+  "img/ordinateur.jpg",
+  "img/photographe.jpg",
+  "img/materiel.jpg",
+  "js/bootstrap.js",
+  "css/font-awesome.css",
+  "/P4_OpenClassroom/style.css",
+  "js/jquery.touchSwipe.js",
+  "img/la-chouette-agence.jpg",
+  "js/blocs.js",
+  "img/logo.png",
+  "css/et-line.css",
+  "js/index.js",
 ];
 
 // mettre en cache
