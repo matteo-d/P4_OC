97afbe29f440bdb03d1921a3f2541951f0f4f938
matteo-d <matteodrouin33@gmail.com>
2021-04-06 16:14:41 +0200

    test

--- js/index.js
+++
@@ -1,6 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("./js/serviceWorker.js")
-      .then(() => console.log("sw registered"))
-      .catch((err) => console.log(err));
-  }
